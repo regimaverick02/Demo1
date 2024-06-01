b9667565e326c8ee2dbd2729376fc33bd5f10fd3
reginold <reginold@maverick02.local>
2024-06-01 11:09:33 -0700

    Add two members

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("This is Member1");
+        System.out.println("This is Member2");
     }
 }
