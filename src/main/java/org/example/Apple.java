47b21efabbd76f8f7f2f79c3732b3b5062c83bbb
Reginold <a.k.reginold@gmail.com>
2024-06-01 11:17:56 -0700

    Add Apple class

---
+++ src/main/java/org/example/Apple.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Apple implements Fruit{
+    public void name() {
+        System.out.println("This is Apple");
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("This is Member1");
         System.out.println("This is Member2");
+        System.out.println("This is Member3");
     }
 }
